3121d5d21f9c8eb224ca3dbff77e54083b0d422a
noodlix <zhibeck271205@gmail.com>
2023-08-01 10:14:35 +0600

    translation started

--- components/home/chat-line.jsx
+++ components/home/chat-line.jsx
@@ -1,14 +1,11 @@
-import {
-  CodeBracketSquareIcon,
-  UserCircleIcon,
-} from "@heroicons/react/24/outline";
+import { PencilIcon, UserCircleIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
 // loading placeholder animation for the chat line
 export const LoadingChatLine = () => (
   <div className="border-b border-black/10 bg-white text-gray-800">
     <div className="relative m-auto flex gap-2 p-4 text-base md:max-w-2xl md:gap-6 md:py-6 lg:max-w-2xl lg:px-0 xl:max-w-3xl">
       <div className="min-w-[30px]">
-        <CodeBracketSquareIcon />
+        <PencilIcon />
       </div>
       <span className="mt-1 animate-pulse cursor-default">▍</span>
     </div>
@@ -41,18 +38,18 @@ export function ChatLine({ role = "assistant", content, cover, isStreaming }) {
     >
       <div className="relative m-auto flex gap-2 p-4 text-base md:max-w-2xl md:gap-6 md:py-6 lg:max-w-2xl lg:px-0 xl:max-w-3xl">
         <div className="min-w-[30px]">
-          {role === "assistant" ? (
-            <CodeBracketSquareIcon />
-          ) : (
-            <UserCircleIcon />
-          )}
+          {role === "assistant" ? <PencilIcon /> : <UserCircleIcon />}
         </div>
 
-        <div className="flex items-center justify-center sm:flex-col md:flex-row">
-          <div className="prose flex-1 whitespace-pre-wrap">
-            {formatteMessage}
+        <div className="flex flex-col sm:flex-row sm:gap-4">
+          <div>
+            <div className="prose flex-1 whitespace-pre-wrap">
+              {formatteMessage}
+            </div>
+            {/* <div className="border-r- mt-6 w-6">
+              <GlobeAltIcon />
+            </div> */}
           </div>
-
           {cover &&
           content !==
             "Give me the name and the author of any book to start!" ? (
